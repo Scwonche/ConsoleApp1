ca1c960da89df077a19a1c9fb13d095a9065c44f
User <kusanjvzhas@mail.ru>
2022-08-14 09:24:34 +0500

    1 версия

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+int n = Int32.Parse(Console.ReadLine() ?? string.Empty);
+if ((n < 10000) | (n > 99999))
+{
+    Console.WriteLine("число не пятизначное");
+}
+else
+{
+
+    if (n % 100 == n / 10000 + n /1000 % 10 * 10)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+string[] arr1= Console.ReadLine().Split();
+ 
+double x1 = double.Parse(arr1[0]);
+double y1 = double.Parse(arr1[1]);
+double z1 = double.Parse(arr1[2]);
+
+string[] arr2= Console.ReadLine()?.Split();
+ 
+double x2 = double.Parse(arr2[0]);
+double y2 = double.Parse(arr2[1]);
+double z2 = double.Parse(arr2[2]);
+double a = Math.Pow(x2 - x1, 2);
+double b = Math.Pow(y2 - y1, 2);
+double c = Math.Pow(z2 - z1, 2);
+Console.WriteLine(Math.Round(Math.Sqrt(a+b+c),2));
