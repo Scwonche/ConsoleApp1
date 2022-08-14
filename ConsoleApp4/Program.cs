92775cb0fb7a24404f391d9a75e8cd153fd82311
User <kusanjvzhas@mail.ru>
2022-08-14 21:02:05 +0500

    урок 4

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int p = 1;
+for (int i = 1; i <= b; i++)
+{
+    p *= a;
+}
+Console.WriteLine(p);
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] array = new int[8];
+Random rnd = new Random();
+for (var i = 0; i <= 7; i++)
+{
+    array[i] = rnd.Next(100);
+}
+var str = string.Join(" ",array);
+Console.WriteLine(str);
